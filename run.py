59bfe459a6453410d6899e0f2e1a413b95abddea
Sahil Kumar <sahilkumar1851320@gmail.com>
2025-09-24 22:45:49 +0530

    update

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-# run.py
 import os
 from app import create_app, db
 from app.models import User, File
 
-# Disable dotenv loading by Flask
+# Disable dotenv
 os.environ['FLASK_SKIP_DOTENV'] = '1'
 
 app = create_app()
 
 # Manual configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = 'dev-secret-key-change-in-production'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 # Auto-create tables on first deploy
 with app.app_context():
@@ -21,4 +21,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'File': File}
 
 if __name__ == '__main__':
-    app.run(debug=True)  # For local testing; Render uses gunicorn
+    app.run()
